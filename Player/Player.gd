aa39b5bbd475796a4aaefb639d5725e15eabd25a
Danideos <34748918+Danideos@users.noreply.github.com>
2022-06-12 21:56:27 +0200

    wall spreading

--- Player/Player.gd
+++ Player/Player.gd
@@ -17,7 +17,7 @@ var TILE_SIZE = 16
 
 # Load scenes
 onready var Main = get_parent().get_parent()
-onready var Grass = preload("res://World/Environment/Grass.tscn").instance()
+#onready var Grass = preload("res://World/Environment/Grass.tscn").instance()
 
 func _physics_process(delta):
 	highlight()
@@ -31,7 +31,7 @@ func _unhandled_input(event):
 	# Check for interactable objects/items 
 	if event.is_action_pressed("ui_interact"):
 		
-		var player_grid_pos = Grass.world_to_map(global_position)
+		var player_grid_pos = Main.Grass.world_to_map(global_position)
 		var items = Main.world_tiles[player_grid_pos.y][player_grid_pos.x]
 		
 		# Snap released item to grid
@@ -58,9 +58,27 @@ func _unhandled_input(event):
 						i.visible = false
 					items[0].visible = true
 					Main.world_tiles[player_grid_pos.y][player_grid_pos.x].clear()
+		else:
+			# Check for walls in direction of last movement
+			# Change player position to center (leg hitbox doesn't work)
+			player_grid_pos = Main.Grass.world_to_map(global_position+Vector2(0,-TILE_SIZE/2))
+			var wall_pos = Vector2(player_grid_pos.x+round(last_direction.x), player_grid_pos.y+round(last_direction.y))
+			var wall = Main.wall_tiles[wall_pos.y][wall_pos.x]
+			if wall and (last_direction.x == 0 or last_direction.y == 0):
+				# Create new walls
+				for i in range(3):
+					for j in range(3):
+						var tile_pos = Vector2(wall_pos.x-1+j,wall_pos.y-1+i)
+						if (!(Main.wall_tiles[tile_pos.y][tile_pos.x]) and 
+						player_grid_pos != tile_pos and 
+						Main.Grass.get_cellv(tile_pos) != 1):
+							Main.spawn_instance("wall", tile_pos, 0)
+							Main.wall_tiles[wall_pos.y][wall_pos.x] = true
+				# Delete old wall -> Fill with corrupt grass
+				Main.spawn_instance("wall", wall_pos, -1)
+				Main.wall_tiles[wall_pos.y][wall_pos.x] = false
+				Main.spawn_instance("grass", wall_pos, 1)
 				
-				
-			
 
 func take_player_input():
 	# Take player direction input
@@ -83,14 +101,13 @@ func update_player_movement(delta):
 
 # Highlight 
 func highlight():
-	var player_grid_pos = Grass.world_to_map(global_position)
+	var player_grid_pos = Main.Grass.world_to_map(global_position)
 	var items = Main.world_tiles[player_grid_pos.y][player_grid_pos.x]
 	# Set highlight if player is on interactable item 
 	if len(items) > 0:
-
 		# Check if item is interactable
-		if items[0].interactable:
-
+		if items[0].get("interactable"):
+			
 			# Remove highlight from old item and add to new
 			if on_item != items[0] and on_item != null:
 				on_item.material.set_shader_param("width", 0.0)
