276383b0083f041d6f54c45fc534815d3f6c4b2d
12334Bruno <brunko.kaiserko@gmail.com>
2022-06-24 13:00:15 +0200

    removed_print

--- Items/ItemGenerators/StoneFormation.gd
+++ Items/ItemGenerators/StoneFormation.gd
@@ -10,7 +10,6 @@ export var time_to_harvest : float = 0.5
 var uses = 5 setget set_uses
 
 func set_uses(value):
-	print(value)
 	if value == 0:
 		var grid_pos = Main.Grass.world_to_map(global_position)
 		Main.world_layers["resource_makers"][grid_pos.y][grid_pos.x].erase(self)
